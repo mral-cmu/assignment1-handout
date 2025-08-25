c40cd05dc0befca2a013f6257195a5c74b76b6bb
Mike A <mikeanoruo2@gmail.com>
2025-08-25 18:28:07 -0400

    Fixed download.py not placing data in correct folder.

--- data/download.py
+++ data/download.py
@@ -1,6 +1,19 @@
+import os
 import gdown
 import subprocess
 
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Change the working directory to the script's directory
+os.chdir(script_dir)
+
+# Download the folder from Google Drive
 gdown.download_folder("https://drive.google.com/drive/folders/1JYbWg0DKEVRHOrldbf5HeOtMCLxwBZXk")
+
+# Move all .npz files from the downloaded folder to the script's directory
 subprocess.run("mv 2023-05-28-07-18-08_npz/*.npz .", shell=True)
+
+# Remove the downloaded folder after moving its contents
 subprocess.run("rm -rf 2023-05-28-07-18-08_npz", shell=True)
+
